3ac1fdc96433e0f36b00600575f800f9f7089b1a
Facug03 <facundogonzales1550@gmail.com>
2024-10-15 11:36:47 -0300

    feat: handle user logged in and not logged in

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,15 +4,37 @@ import { css } from '@styled-system/css'
 import { hstack } from '@styled-system/patterns'
 import Link from 'next/link'
 
-export function Header() {
+import { signOut } from '@/actions/log-out'
+import { createClient } from '@/utils/supabase/server'
+
+export async function Header() {
+  const supabase = createClient()
+  const {
+    data: { user }
+  } = await supabase.auth.getUser()
+
   return (
     <header className={css({ paddingY: 2 })}>
       <nav className={hstack({ justify: 'space-between' })}>
         <Heading as="p">Spooky Pics</Heading>
 
-        <Button asChild>
-          <Link href="/login">Log in</Link>
-        </Button>
+        {!user && (
+          <Button asChild rounded="full">
+            <Link href="/login">Log in</Link>
+          </Button>
+        )}
+
+        {user && (
+          <div className={hstack({ gap: 2 })}>
+            <Button asChild variant="outline" rounded="full">
+              <Link href="/upload">Upload</Link>
+            </Button>
+
+            <form action={signOut}>
+              <Button rounded="full">Log out</Button>
+            </form>
+          </div>
+        )}
       </nav>
     </header>
   )
