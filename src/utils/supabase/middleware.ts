6b29483b4e1a17c3369c5439911e540b1cde7f6f
Facug03 <facundogonzales1550@gmail.com>
2024-10-15 11:35:01 -0300

    feat: add auth middleware

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -3,6 +3,9 @@ import { type NextRequest, NextResponse } from 'next/server'
 
 import { config } from '@/config'
 
+const restrictedAuthRoutes = ['/login']
+const protectedRoutes = ['/upload']
+
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request
@@ -31,37 +34,25 @@ export async function updateSession(request: NextRequest) {
     }
   )
 
-  // IMPORTANT: Avoid writing any logic between createServerClient and
-  // supabase.auth.getUser(). A simple mistake could make it very hard to debug
-  // issues with users being randomly logged out.
-
   const {
-    data: { user }
+    data: { user },
+    error
   } = await supabase.auth.getUser()
+  const currentPath = request.nextUrl.pathname
 
-  if (
-    !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth')
-  ) {
-    // no user, potentially respond by redirecting the user to the login page
-    const url = request.nextUrl.clone()
-    url.pathname = '/login'
-    return NextResponse.redirect(url)
+  const isRestrictedAuthRoutes = restrictedAuthRoutes.includes(currentPath)
+
+  if (user && isRestrictedAuthRoutes) {
+    return NextResponse.redirect(new URL('/', request.url))
   }
 
-  // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
-  // creating a new response object with NextResponse.next() make sure to:
-  // 1. Pass the request in it, like so:
-  //    const myNewResponse = NextResponse.next({ request })
-  // 2. Copy over the cookies, like so:
-  //    myNewResponse.cookies.setAll(supabaseResponse.cookies.getAll())
-  // 3. Change the myNewResponse object to fit your needs, but avoid changing
-  //    the cookies!
-  // 4. Finally:
-  //    return myNewResponse
-  // If this is not done, you may be causing the browser and server to go out
-  // of sync and terminate the user's session prematurely!
+  const isProtectedRoute = protectedRoutes.includes(currentPath)
+
+  if (!isProtectedRoute) return supabaseResponse
+
+  if (!user || error) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
 
   return supabaseResponse
 }
