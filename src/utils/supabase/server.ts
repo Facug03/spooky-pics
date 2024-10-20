20d78871dabcc75b8860df4249e54e21bdaa8706
Facug03 <facundogonzales1550@gmail.com>
2024-10-20 15:17:24 -0300

    feat: add types to db

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -2,30 +2,27 @@ import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 import { config } from '@/config'
+import type { Database } from '@/types/supabase'
 
 export function createClient() {
   const cookieStore = cookies()
 
-  return createServerClient(
-    config.NEXT_PUBLIC_SUPABASE_URL ?? '',
-    config.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
-    {
-      cookies: {
-        getAll() {
-          return cookieStore.getAll()
-        },
-        setAll(cookiesToSet) {
-          try {
-            for (const { name, options, value } of cookiesToSet) {
-              cookieStore.set(name, value, options)
-            }
-          } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
+  return createServerClient<Database>(config.NEXT_PUBLIC_SUPABASE_URL ?? '', config.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '', {
+    cookies: {
+      getAll() {
+        return cookieStore.getAll()
+      },
+      setAll(cookiesToSet) {
+        try {
+          for (const { name, options, value } of cookiesToSet) {
+            cookieStore.set(name, value, options)
           }
+        } catch {
+          // The `setAll` method was called from a Server Component.
+          // This can be ignored if you have middleware refreshing
+          // user sessions.
         }
       }
     }
-  )
+  })
 }
