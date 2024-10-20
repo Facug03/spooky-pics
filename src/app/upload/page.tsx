ef1618d0bc1e70fcbf59fd7d580fa594c300c236
Facug03 <facundogonzales1550@gmail.com>
2024-10-20 15:13:17 -0300

    feat: pass tags to client component

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -2,7 +2,7 @@ import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
 
 import { stack } from '@styled-system/patterns'
-import { Steps } from './sections/Steps'
+import { Steps } from './sections/steps'
 
 export default async function Upload() {
   const supabase = createClient()
@@ -15,9 +15,11 @@ export default async function Upload() {
     return redirect('/login')
   }
 
+  const { data } = await supabase.from('tag').select('*').order('name', { ascending: true })
+
   return (
     <main className={stack({ paddingY: '10' })}>
-      <Steps />
+      <Steps tags={data ?? []} />
     </main>
   )
 }
