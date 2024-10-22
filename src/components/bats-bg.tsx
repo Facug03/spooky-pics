3d755738a8aa3c345a8ecf6208f79993213918a2
Facug03 <facundogonzales1550@gmail.com>
2024-10-22 12:52:10 -0300

    fix: make bg image smaller

--- src/components/bats-bg.tsx
+++ src/components/bats-bg.tsx
@@ -5,7 +5,20 @@ import { css } from '@styled-system/css'
 export function BatsBg() {
   return (
     <div className={css({ position: 'absolute', top: 0, right: 0, zIndex: '-100' })}>
-      <Image src="/bats.webp" width={500} height={500} alt="Bats background image" />
+      <div
+        className={css({
+          position: 'relative',
+          width: '70%',
+          height: 'auto',
+          marginLeft: 'auto',
+          md: {
+            width: 500,
+            height: 500
+          }
+        })}
+      >
+        <Image src="/bats.webp" width={500} height={500} alt="Bats background image" />
+      </div>
     </div>
   )
 }
